e6160dae943bd3c3ec23cce97983d24b6ce622f2
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2021-07-02 02:57:02 +0000

    [homesのみ]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-
+  
+  before_action :authenticate_user!,except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
   def after_sign_in_path_for(resource)
     user_path(@user)
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    homes_about_path
   end
 
   def configure_permitted_parameters
