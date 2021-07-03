b80d8cdd040248894ebdcaab1b43c61b523f149f
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2021-07-03 04:38:22 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    homes_about_path
+    root_path
   end
 
   def configure_permitted_parameters
